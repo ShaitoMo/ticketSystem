d6bb4467c9a006f967b95621b2b71735a4df2cdc
ShaitoMo <shaitomohammad695@gmail.com>
2025-10-04 12:08:28 +0300

    Update sms.php

--- sms.php
+++ sms.php
@@ -4,16 +4,16 @@ require_once 'vendor/autoload.php';
 
 use Twilio\Rest\Client;
 
-$sid    = "ACf909be81510c3bd2fc9e32cfdd1122b3"; // Your Account SID
-$token  = "b39c65e3b36824f9f5ef701c1796a868"; // Your Auth Token
+$sid   
+$token  // Your Auth Token
 
 try {
     $twilio = new Client($sid, $token);
 
     $message = $twilio->messages->create(
-        "+96178838334", // Recipient phone number
+        "", // Recipient phone number
         [
-            "from" => "+17573514865", // Twilio phone number
+            "from" => "", // Twilio phone number
             "body" => "why" // Message content
         ]
     );
